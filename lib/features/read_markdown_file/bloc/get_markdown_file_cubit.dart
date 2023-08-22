e2fbc6fd943f0d58cba017462aa17bd507a801bb
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-08-22 09:56:36 +0100

    wip: add get markdown file cubit and tests

---
+++ lib/features/read_markdown_file/bloc/get_markdown_file_cubit.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:markdown_to_flashcard/features/read_markdown_file/bloc/get_markdown_file_state.dart';
+import 'package:markdown_to_flashcard/features/read_markdown_file/data/repositories/markdown_file_repository.dart';
+
+class GetMarkdownFileCubit extends Cubit<GetMarkdownFileState> {
+  final MarkdownFileRepository repository;
+
+  GetMarkdownFileCubit({required this.repository})
+      : super(const GetMarkdownFileState());
+
+  Future<void> getMarkdownFile() async {
+    emit(state.copyWith(status: GetMarkdownFileStatus.loading));
+
+    try {
+      final File? file = await repository.getMarkdownFile();
+
+      emit(
+        state.copyWith(
+          status: GetMarkdownFileStatus.success,
+          file: file,
+        ),
+      );
+    } on Exception catch (exception) {
+      emit(
+        state.copyWith(
+          status: GetMarkdownFileStatus.failure,
+          exception: exception,
+        ),
+      );
+    }
+  }
+}
---
+++ test/features/read_markdown_file/bloc/get_markdown_file_cubit_test.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:markdown_to_flashcard/features/read_markdown_file/bloc/get_markdown_file_cubit.dart';
+import 'package:markdown_to_flashcard/features/read_markdown_file/bloc/get_markdown_file_state.dart';
+import 'package:markdown_to_flashcard/features/read_markdown_file/data/repositories/markdown_file_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockMarkdownFileRepository extends Mock
+    implements MarkdownFileRepository {}
+
+void main() {
+  late MockMarkdownFileRepository mock;
+  late GetMarkdownFileCubit cubit;
+
+  setUp(() {
+    mock = MockMarkdownFileRepository();
+    cubit = GetMarkdownFileCubit(repository: mock);
+  });
+
+  group('getMarkdownFile()', () {
+    final File expected = File('test.md');
+    final Exception exception = Exception('Failed to get Markdown file.');
+
+    blocTest<GetMarkdownFileCubit, GetMarkdownFileState>(
+      'GIVEN the user successfully selects a Markdown file, '
+      "WHEN 'getMarkdownFile()' is called from the cubit, "
+      "THEN call 'getMarkdownFile()' from the repository, "
+      'AND emit [GetMarkdownFileStatus.loading, GetMarkdownFileStatus.success]',
+      setUp: () =>
+          when(() => mock.getMarkdownFile()).thenAnswer((_) async => expected),
+      build: () => cubit,
+      act: (cubit) => cubit.getMarkdownFile(),
+      verify: (_) async => verify(() => mock.getMarkdownFile()).called(1),
+      expect: () => <GetMarkdownFileState>[
+        const GetMarkdownFileState(status: GetMarkdownFileStatus.loading),
+        GetMarkdownFileState(
+          status: GetMarkdownFileStatus.success,
+          file: expected,
+        ),
+      ],
+    );
+
+    blocTest<GetMarkdownFileCubit, GetMarkdownFileState>(
+      'GIVEN an exception occurs when user selects a file, '
+      "WHEN 'getMarkdownFile()' is called from the cubit, "
+      "THEN call 'getMarkdownFile()' from the repository, "
+      'AND emit [GetMarkdownFileStatus.loading, GetMarkdownFileStatus.failure]',
+      setUp: () => when(() => mock.getMarkdownFile()).thenThrow(exception),
+      build: () => cubit,
+      act: (cubit) => cubit.getMarkdownFile(),
+      verify: (_) async => verify(() => mock.getMarkdownFile()).called(1),
+      expect: () => <GetMarkdownFileState>[
+        const GetMarkdownFileState(status: GetMarkdownFileStatus.loading),
+        GetMarkdownFileState(
+          status: GetMarkdownFileStatus.failure,
+          exception: exception,
+        ),
+      ],
+    );
+  });
+}
