f717eb7af6bc8f2ad234419576485feef8362984
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-09-20 10:02:25 +0100

    fix: fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:markdown_to_flashcard/features/read_markdown_file/domain/use_cases/request_ankidroid_permission_use_case.dart';
-import 'package:markdown_to_flashcard/features/read_markdown_file/presentation/bloc/get_markdown_file_cubit.dart';
 
+import 'features/read_markdown_file/presentation/bloc/markdown_to_flashcard_cubit.dart';
 import 'features/read_markdown_file/presentation/get_markdown_file_screen.dart';
 import '../../../injection_container.dart' as di;
 
