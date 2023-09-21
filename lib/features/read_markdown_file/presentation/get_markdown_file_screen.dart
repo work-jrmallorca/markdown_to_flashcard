b2fc0e82e42269f053e3ab5da6fa4a4290243c6b
Jonquil Isys Mallorca <102964499+work-jrmallorca@users.noreply.github.com>
2023-09-21 10:46:40 +0100

    wip: apply main screen UI

--- lib/features/read_markdown_file/presentation/get_markdown_file_screen.dart
+++ lib/features/read_markdown_file/presentation/get_markdown_file_screen.dart
@@ -1,4 +1,6 @@
+import 'package:drop_shadow/drop_shadow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'bloc/markdown_to_flashcard_cubit.dart';
@@ -9,16 +11,27 @@ class GetMarkdownFileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Markdown to Flashcards'),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 16.0, top: 16.0),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: buildBody(context),
+      body: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: buildBody(context),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Select a Markdown File'),
         onPressed: () =>
             context.read<MarkdownToFlashcardCubit>().getMarkdownFile(),
-        label: const Text('Select a Markdown file'),
       ),
     );
   }
@@ -48,13 +61,61 @@ class GetMarkdownFileScreen extends StatelessWidget {
         } else if (state.status == GetMarkdownFileStatus.loading) {}
       },
       builder: (context, state) {
-        switch (state.status) {
-          case GetMarkdownFileStatus.loading:
-            return const Center(child: CircularProgressIndicator());
-          default:
-            return Container();
-        }
+        return Column(
+          children: [
+            Expanded(child: _buildImage(context, state)),
+            Expanded(child: _buildDescription()),
+          ],
+        );
       },
+    );
+  }
+
+  Widget _buildImage(BuildContext context, MarkdownToFlashcardState state) {
+    switch (state.status) {
+      case GetMarkdownFileStatus.initial:
+        return Center(
+          child: DropShadow(
+            offset: const Offset(8.0, 8.0),
+            child: Image.asset(
+              'assets/images/anki.png',
+              scale: 5.0,
+            ),
+          ),
+        );
+      case GetMarkdownFileStatus.loading:
+        return const Center(child: CircularProgressIndicator());
+      default:
+        return Container();
+    }
+  }
+
+  Widget _buildDescription() {
+    return const Column(
+      children: [
+        Text(
+          'Import to Anki',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 36.0,
+            fontWeight: FontWeight.w700,
+          ),
+          maxLines: 2,
+        ),
+        SizedBox(height: 25.0),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Text(
+            'Select a markdown file to import your flashcards from.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 18.0,
+            ),
+            maxLines: 2,
+          ),
+        ),
+      ],
     );
   }
 }
